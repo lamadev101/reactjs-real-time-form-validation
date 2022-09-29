477efc1333019c958a472e324a518382e694acb1
Sworinm-Karma <ghisingkarma740@gmail.com>
2022-09-29 18:54:59 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import InputForm from './components/InputForm';
 
 function App() {
+  const [values, setValues] = useState({
+    fullname: "",
+    email: "",
+    mobile: "",
+    birthdate: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs =[
+    {
+      name: 'fullname', 
+      type: 'text', 
+      placeholder: 'Full Name', 
+      label: 'Full Name',
+      pattern: `[a-zA-Z]{3,16}[ ]{0,1}[a-zA-z]{3,16}$`, 
+      errorMsg: "Provide the valid full name!!", 
+      required: true
+    },
+    {
+      name: 'email', 
+      type: 'email', 
+      placeholder: 'Email', 
+      label: 'Email', 
+      errorMsg: "Email address is required.", 
+      required: true
+    },
+    {
+      name: 'mobile', 
+      type: 'number', 
+      placeholder: 'Mobile Number', 
+      label: 'Mobile Number', 
+      errorMsg: "Phone number must be provide!!", 
+      required: true
+    },
+    {
+      name: 'birthdate', 
+      type: 'date', 
+      placeholder: 'Date of Birth', 
+      label: 'Date of Birth', 
+      errorMsg: "Date of Birth must be enter!!", 
+      required: true
+    },
+    {
+      name: 'password', 
+      type: 'password', 
+      placeholder: 'Password', 
+      label: 'Password', 
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`, 
+      errorMsg:"Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!", 
+      required: true
+    },
+    {
+      name: 'confirmedPassword', 
+      type: 'password', 
+      placeholder: 'Confirm Password', 
+      pattern: values.password, 
+      label: 'Comfirm Password', 
+      errorMsg: "Password does not match!!", 
+      required: true
+    },
+  ]
+
+  const handleSubmit = (e) => {
+    e.prevantDefault();
+  }
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Create Account</h1>
+        {inputs.map((input, idx)=>(
+          <InputForm key={idx} {...input} onChange={onChange} />
+        ))}
+        <button>Create Account</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react';
+import './InputForm.css';
+
+const InputForm = (props) => {
+  const[focused, setFocused] = useState(false);
+  const {label, onChange, errorMsg, ...inputProps} = props;
+  const handleFocus = (e) => {
+    setFocused(true);
+  }
+  return (
+    <div className='formAttr'>
+        <label htmlFor="">{label}:</label>
+        <input {...inputProps} onChange={onChange} onBlur={handleFocus} onFocus={()=>inputProps.name === "confirmPassword" && setFocused(true)} focused={focused.toString()} />
+        <span>{errorMsg}</span>
+    </div>
+  )
+}
+
+export default InputForm
